a9884527f4b5e7b0afa3568502d108e4b4fe36a3
nidakkabak <akkabaknida1@example.com>
2025-07-10 12:23:32 +0300

    commit

--- yemek-sistemi/src/components/MenuViewer.js
+++ yemek-sistemi/src/components/MenuViewer.js
@@ -12,7 +12,7 @@ export default function MenuViewer({ initialMenu, alternatives }) {
     setEditingIndex(index);
     setSelectedCategory("");
     setSelectedOption(null);
-  };
+  };   
 
   const handleSave = () => {
     if (!selectedOption) return;
